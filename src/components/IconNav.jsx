cd22e2d5204e4dd1fa4e6d79e3a07bb4ce5d2d3d
Tim Hilton <tim@timhilton.net>
2022-05-17 18:13:57 -0700

    links to resume

--- src/components/IconNav.jsx
+++ src/components/IconNav.jsx
@@ -13,7 +13,10 @@ export default class IconNav extends PureComponent {
             <section className="icon-nav">
                 <Iconpathline className="icon-pathline"/>
                 <nav className="icon-nav-container">
-                    <Link to="/contact" className="icons icon-resume"><Resume/></Link>
+                    <Link to="#" 
+                    onClick={(e) => {
+                        window.location.href = 'https://s3.amazonaws.com/www.timhilton.net/TimHiltonResume2021.pdf'
+                    }} className="icons icon-resume"><Resume/></Link>
                     <Link to="#" onClick={(e) => {
                         window.location.href = 'mailto:tim@timhilton.net';
                         e.preventDefault();
