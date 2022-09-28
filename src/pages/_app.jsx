a25715da42e45726007c9833b7533c6278112d10
Tim Hilton <tim@timhilton.net>
2022-09-27 20:47:36 -0700

    sets global styles

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,9 +1,53 @@
 import { useRouter } from 'next/router';
 import Script from 'next/script';
 import { useEffect } from 'react';
-import '../../styles/globals.css'
+import { createGlobalStyle } from 'styled-components';
 import Layout from '../components/Layout'
 import * as ga from '../utils/gtm';
+
+const GlobalStyle = createGlobalStyle`
+@font-face {
+  font-family: 'Galano';
+  src: local("GalanoGrotesque-Extra-Light"), local("GalanoGrotesque-Extra-Light"), url("/fonts/GalanoGrotesque-Extra-Light.otf") format("opentype");
+  font-weight: 100;
+  font-style: normal; 
+}
+
+@font-face {
+  font-family: 'Galano';
+  src: local("GalanoGrotesque Italic"), local("GalanoGrotesque-Italic"), url("/fonts/GalanoGrotesque-Italic.otf") format("opentype");
+  font-weight: normal;
+  font-style: italic; 
+}
+
+@font-face {
+  font-family: 'SF Pro Text';
+  src: local("SF-Pro-Text-LightItalic"), local("SF-Pro-Text-LightItalic"), url("/fonts/SF-Pro-Text-LightItalic.otf") format("opentype");
+  font-weight: 300;
+  font-style: italic; 
+}
+
+html,
+body {
+  padding: 0;
+  margin: 0;
+  font-family: 'Galano';
+  overflow-x: hidden;
+}
+
+h1 {
+  font-size: 3.5em;
+}
+
+a {
+  color: inherit;
+  text-decoration: none;
+}
+
+* {
+  box-sizing: border-box;
+}
+`
 
 
 function MyApp({ Component, pageProps }) {
@@ -38,6 +82,8 @@ function MyApp({ Component, pageProps }) {
         gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
         `}
       </Script>
+      <GlobalStyle/>
+      
       <Layout>
         <Component {...pageProps} />
       </Layout>
--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,10 +1,58 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        })
+
+      const initialProps = await Document.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      }
+    } finally {
+      sheet.seal()
+    }
+  }
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <link
+            rel="preload"
+            href="/fonts/GalanoGrotesque-Extra-Light.otf"
+            as="font"
+            type="font/otf"
+            crossOrigin=""
+          />
+          <link
+            rel="preload"
+            href="/fonts/GalanoGrotesque-Italic.otf"
+            as="font"
+            type="font/otf"
+            crossOrigin=""
+          />
+          <link
+            rel="preload"
+            href="/fonts/SF-Pro-Text-LightItalic.otf"
+            as="font"
+            type="font/otf"
+            crossOrigin=""
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
