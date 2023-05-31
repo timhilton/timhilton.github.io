055c7f6d15926bc4d2de48ccedef30cb06161536
Tim Hilton <tim@timhilton.net>
2023-05-30 23:00:40 -0700

    attempt to stop redirect 6

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -127,7 +127,7 @@ export default function Contact() {
         fetch("/", {
             method: "POST",
             headers: { "Content-Type": "application/x-www-form-urlencoded" },
-            body: encode({ "form-name": "contact", ...data })
+            body: encode({ "form-name": "contact", ...formData })
           })
             .then(() => console.log("Success!"))
             .catch(error => console.log(error));
