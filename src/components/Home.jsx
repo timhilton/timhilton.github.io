f351ab65effbe1141671ed2697a2ab04c025335d
Tim Hilton <tim@timhilton.net>
2023-06-04 21:28:44 -0700

    minor syntax changes and code cleanup - also adds blurb to home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,4 @@
-import { useEffect, useState } from "react";
-import { useRouter } from "next/router";
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
 import Ampersand from "../svgs/ampersand.svg";
 
 // Calculate the color value outside the styled component
@@ -10,6 +8,7 @@ const Section = styled.section`
     align-items: center;
     color: rgb(${textColor});
     display: flex;
+    flex-direction: column;
     height: 100%;
     justify-content: center;
     opacity: 1;
@@ -17,10 +16,10 @@ const Section = styled.section`
 `;
 
 const Headings = styled.div`
-    font-style: italic;
-    font-weight: normal;
     color: rgb(${textColor});
     display: grid;
+    font-style: italic;
+    font-weight: normal;
     grid-template-areas: 
         "amp top-heading"
         "amp bottom-heading";
@@ -55,34 +54,37 @@ const Headings = styled.div`
 `;
 
 const Amp = styled(Ampersand)`
-    width: 125px;
-    height: 140px;
     grid-area: amp;
+    height: 140px;
     path {
         fill: rgba(${textColor}, 0.4);
     }
+    width: 125px;
 
     @media (max-width: 734px) {
-        width: 78px;
         height: 87px;
+        width: 78px;
     }
 `;
 
 const Copy = styled.p`
+    background-color: rgba(151, 151, 151, 0.7);
+    border-radius: 20px;
+    box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.5);
+    color: rgb(${textColor});
+    font-size: 1.5em; /* 24px / 16px = 1.5em */
     font-style: normal;
     font-weight: 100;
-    width: 505px;
-    font-size: 1.5em; /* 24px / 16px = 1.5em */
-    line-height: 2.5em; /* 40px / 16px = 2.5em */
-    color: rgb(${textColor});
+    padding: 1em 1.5em;
+    width: 87.5%;
 
     @media (max-width: 734px) {
         font-size: 1.125em; /* 18px / 16px = 1.125em */
-        line-height: 2.25em; /* 36px / 16px = 2.25em */
         width: 100%;
-        align-self: flex-end;
     }
+}
 `;
+
 
 const Home = () => {
     return (
@@ -92,6 +94,9 @@ const Home = () => {
                 <h3>Web Development</h3>
                 <h3>Creative Services</h3>
             </Headings>
+            <Copy>
+            I'm an experienced Web Engineer focused on Front End Development working predominately with JavaScript, React, Node, and Sass.  I have a demonstrated history of working and creating award winning web experiences in the marketing/advertising, and entertainment industries.
+            </Copy>
         </Section>
     );
 };
