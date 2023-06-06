58c89e758bece127f4fae82d32bbadd47c7ae730
Tim Hilton <tim@timhilton.net>
2023-06-05 22:37:27 -0700

    might've fixed it who knows

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Head from 'next/head';
 import Home from '../components/Home';
-import ContentfulApi from "../utils/ContentfulApi";
+import ContentfulApi from "../utils/ContentfulApi.js";
 
 export async function getStaticProps() {
   const homeContent = await ContentfulApi.getHomeContent();
